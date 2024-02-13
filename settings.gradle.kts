6aec4ce783835af6a96b1a3cef9f50ae9c086026
Yacine Rezgui <yrezgui@google.com>
2024-02-13 10:28:42 +0100

    Add files generated by Android Studio

---
+++ settings.gradle.kts
@@ -0,0 +1,18 @@
+pluginManagement {
+    repositories {
+        google()
+        mavenCentral()
+        gradlePluginPortal()
+    }
+}
+dependencyResolutionManagement {
+    repositoriesMode.set(RepositoriesMode.FAIL_ON_PROJECT_REPOS)
+    repositories {
+        google()
+        mavenCentral()
+    }
+}
+
+rootProject.name = "EmbedTest"
+include(":picker")
+include(":host")
